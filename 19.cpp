5aec56c937aea9e59c52ce4a06b028e3dbe2587c
yaolele05 <3256408162@qq.com>
2026-03-24 21:20:52 +0800

    19

---
+++ 19.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<vector<char>> mm(n,vector<char>(m));
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+        cin>>mm[i][j];
+        }
+    }
+ int sum=0;
+ for(int i=1;i<n-1;i++)
+ {
+    int b=1;
+  for(int j=1;j<m-1;j++)
+  {
+    int b=1;
+   
+    while(mm[i][j]==mm[i-b][j-b]&&mm[i][j]==mm[i+b][j+b]&&mm[i][j]==mm[i-b][j+b]&&mm[i][j]==mm[i+b][j-b])
+    {
+    sum++;
+    b++;
+    }
+  }
+      
+
+ }
+ cout<<sum<<endl;
+    return 0;
+}
